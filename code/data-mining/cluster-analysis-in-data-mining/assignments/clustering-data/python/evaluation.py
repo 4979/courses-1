a13cb1742b2221d4a32e244e0d7f935599ad84dd
iamprabhat <prabhat.genome@gmail.com>
2015-09-21 20:26:24 +0530

    Initial commit
    Programming Assignment Data, from University of Illinois at
    Urbana-Champaign, USA.

---
+++ code/data-mining/cluster-analysis-in-data-mining/assignments/clustering-data/python/evaluation.py
@@ -0,0 +1,16 @@
+from math import log, sqrt
+
+def  purity(groundtruthAssignment, algorithmAssignment):
+
+    purity = 0
+    # TODO  
+    # Compute the purity 
+    return purity 
+
+def NMI(groundtruthAssignment, algorithmAssignment):
+
+    NMI = 0
+    # TODO
+    # Compute the NMI
+
+    return NMI
